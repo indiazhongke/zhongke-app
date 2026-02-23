c547dca7204ec702d80ac5c4734f2c8f35fe5a47
indiazhongke <indiazhongke@gmail.com>
2026-02-23 14:01:55 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://zhongke-app-zgf2.onrender.com",
+  baseURL: "https://zhongke-app-zgf2.onrender.com/api",
 });
 
 export default API;
