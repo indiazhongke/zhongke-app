2c9f83a6eea51d48d00ae2423562c738c261c019
indiazhongke <indiazhongke@gmail.com>
2026-02-24 16:41:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,26 +7,41 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: "autoUpdate",
-      includeAssets: ["logox.png"],
+      includeAssets: [
+        "logox192.png",
+        "logox512.png",
+        "logox.png"
+      ],
+
+      devOptions: {
+        enabled: true
+      },
+
       manifest: {
         name: "Zhongke India Taskflow Manager",
         short_name: "Zhongke CRM",
         description: "Internal Task Management CRM",
+
         theme_color: "#1c7ab8",
         background_color: "#ffffff",
+
         display: "standalone",
         orientation: "portrait",
         start_url: "/",
+        scope: "/",
+
         icons: [
           {
-            src: "/logox.png",
+            src: "/logox192.png",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/png",
+            purpose: "any maskable"
           },
           {
-            src: "/logox.png",
+            src: "/logox512.png",
             sizes: "512x512",
-            type: "image/png"
+            type: "image/png",
+            purpose: "any maskable"
           }
         ]
       }
