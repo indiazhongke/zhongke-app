52861cab08926f03a6c681081587e8a82486a801
indiazhongke <indiazhongke@gmail.com>
2026-02-24 15:25:07 +0530

    Update Task.js

--- zhongke-backend/models/Task.js
+++ zhongke-backend/models/Task.js
@@ -17,11 +17,5 @@ const taskSchema = new mongoose.Schema(
   },
   { timestamps: true },
 );
-taskSchema.set("toJSON", {
-  transform: (doc, ret) => {
-    ret.id = ret._id;
-    delete ret._id;
-    delete ret.__v;
-  }
-});
+
 module.exports = mongoose.model("Task", taskSchema);
